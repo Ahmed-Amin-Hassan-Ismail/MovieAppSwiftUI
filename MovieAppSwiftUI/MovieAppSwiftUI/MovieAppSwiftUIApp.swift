121277e85c074a65b8de7480a7aad3c8f90c1825
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-04-27 06:05:48 +0200

    initial commit create xCode project

---
+++ MovieAppSwiftUI/MovieAppSwiftUI/MovieAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppSwiftUIApp.swift
+//  MovieAppSwiftUI
+//
+//  Created by Ahmed Amin on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
